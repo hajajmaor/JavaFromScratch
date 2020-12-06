a0943129cd36ceb691dcf994d3e05bf02d9e6d1b
hajajmaor <hajajmaor@gmail.com>
2020-12-06 16:43:32 +0200

    add new functions

--- src/basic_java/CodingProblems.java
+++ src/basic_java/CodingProblems.java
@@ -2,7 +2,7 @@ package basic_java;
 
 public class CodingProblems {
     /**
-     * This problem was asked by Glassdoor.
+     * TODO: This problem was asked by Glassdoor.
      * 
      * An imminent hurricane threatens the coastal town of Codeville. If at most two
      * people can fit in a rescue boat, and the maximum weight limit for a given
@@ -14,6 +14,32 @@ public class CodingProblems {
      * @param arr   - array of weights of peoples
      * @param limit -limit of weight per boat
      */
-    public static void problem291(int[] arr, int limit) {
+    public static int problem291(int[] arr, int limit) {
+        return -1;
     }
+
+    /**
+     * TODO: This problem was asked by Salesforce.
+     * 
+     * The number 6174 is known as Kaprekar's contant, after the mathematician who
+     * discovered an associated property: for all four-digit numbers with at least
+     * two distinct digits, repeatedly applying a simple procedure eventually
+     * results in this value. The procedure is as follows:
+     * 
+     * For a given input x, create two new numbers that consist of the digits in x
+     * in ascending and descending order. Subtract the smaller number from the
+     * larger number. For example, this algorithm terminates in three steps when
+     * starting from 1234:
+     * 
+     * 4321 - 1234 = 3087 8730 - 0378 = 8352 8532 - 2358 = 6174 Write a function
+     * that returns how many steps this will take for a given input N.
+     */
+    public static int problem288(int x) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        assert (problem288(1234) == 3);
+    }
+
 }
